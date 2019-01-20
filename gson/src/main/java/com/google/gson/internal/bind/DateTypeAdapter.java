7385b5e33d086dbc774c4240ea24474f209aad1a
Renato Oliveira <rend.master@gmail.com>
2019-01-20 12:31:37 -0300

    dell inspiron recs 50k

--- gson/src/main/java/com/google/gson/internal/bind/DateTypeAdapter.java
+++ gson/src/main/java/com/google/gson/internal/bind/DateTypeAdapter.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.google.gson.internal.bind;
 
 import com.google.gson.Gson;
@@ -27,7 +26,6 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 import com.google.gson.util.VersionUtils;
-
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -44,58 +42,63 @@ import java.util.Locale;
  * to synchronize its read and write methods.
  */
 public final class DateTypeAdapter extends TypeAdapter<Date> {
-  public static final TypeAdapterFactory FACTORY = new TypeAdapterFactory() {
-    @SuppressWarnings("unchecked") // we use a runtime check to make sure the 'T's equal
-    @Override public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> typeToken) {
-      return typeToken.getRawType() == Date.class ? (TypeAdapter<T>) new DateTypeAdapter() : null;
-    }
-  };
 
-  /**
-   * List of 1 or more different date formats used for de-serialization attempts.
-   * The first of them (default US format) is used for serialization as well.
-   */
-  private final List<DateFormat> dateFormats = new ArrayList<DateFormat>();
+    public static final TypeAdapterFactory FACTORY = new TypeAdapterFactory() {
 
-  public DateTypeAdapter() {
-    dateFormats.add(DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, Locale.US));
-    if (!Locale.getDefault().equals(Locale.US)) {
-      dateFormats.add(DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT));
-    }
-    if (VersionUtils.isJava9OrLater()) {
-      dateFormats.add(PreJava9DateFormatProvider.getUSDateTimeFormat(DateFormat.DEFAULT, DateFormat.DEFAULT));
-    }
-  }
+        // we use a runtime check to make sure the 'T's equal
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> typeToken) {
+            return typeToken.getRawType() == Date.class ? (TypeAdapter<T>) new DateTypeAdapter() : null;
+        }
+    };
 
-  @Override public Date read(JsonReader in) throws IOException {
-    if (in.peek() == JsonToken.NULL) {
-      in.nextNull();
-      return null;
-    }
-    return deserializeToDate(in.nextString());
-  }
+    /**
+     * List of 1 or more different date formats used for de-serialization attempts.
+     * The first of them (default US format) is used for serialization as well.
+     */
+    private final List<DateFormat> dateFormats = new org.eclipse.collections.impl.list.mutable.FastList<DateFormat>();
 
-  private synchronized Date deserializeToDate(String json) {
-    for (DateFormat dateFormat : dateFormats) {
-      try {
-        return dateFormat.parse(json);
-      } catch (ParseException ignored) {}
+    public DateTypeAdapter() {
+        dateFormats.add(DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, Locale.US));
+        if (!Locale.getDefault().equals(Locale.US)) {
+            dateFormats.add(DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT));
+        }
+        if (VersionUtils.isJava9OrLater()) {
+            dateFormats.add(PreJava9DateFormatProvider.getUSDateTimeFormat(DateFormat.DEFAULT, DateFormat.DEFAULT));
+        }
     }
-    try {
-    	return ISO8601Utils.parse(json, new ParsePosition(0));
-    } catch (ParseException e) {
-      throw new JsonSyntaxException(json, e);
-    }
-  }
 
-  @Override public synchronized void write(JsonWriter out, Date value) throws IOException {
-    if (value == null) {
-      out.nullValue();
-      return;
+    @Override
+    public Date read(JsonReader in) throws IOException {
+        if (in.peek() == JsonToken.NULL) {
+            in.nextNull();
+            return null;
+        }
+        return deserializeToDate(in.nextString());
     }
-    String dateFormatAsString = dateFormats.get(0).format(value);
-    out.value(dateFormatAsString);
-  }
-  
-  
+
+    private synchronized Date deserializeToDate(String json) {
+        for (DateFormat dateFormat : dateFormats) {
+            try {
+                return dateFormat.parse(json);
+            } catch (ParseException ignored) {
+            }
+        }
+        try {
+            return ISO8601Utils.parse(json, new ParsePosition(0));
+        } catch (ParseException e) {
+            throw new JsonSyntaxException(json, e);
+        }
+    }
+
+    @Override
+    public synchronized void write(JsonWriter out, Date value) throws IOException {
+        if (value == null) {
+            out.nullValue();
+            return;
+        }
+        String dateFormatAsString = dateFormats.get(0).format(value);
+        out.value(dateFormatAsString);
+    }
 }
